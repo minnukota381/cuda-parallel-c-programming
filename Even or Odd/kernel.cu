7233667706e40cc0602b2227edc29b24204e4f84
Minnu <110650599+minnukota381@users.noreply.github.com>
2023-11-23 11:27:35 +0530

    Update kernel.cu

--- Even or Odd/kernel.cu
+++ Even or Odd/kernel.cu
@@ -1,1 +1,25 @@
 
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void evenOrOdd(int num) {
+    if (num % 2 == 0) {
+        printf("%d is even\n", num);
+    }
+    else {
+        printf("%d is odd\n", num);
+    }
+}
+
+int main() {
+    int num;
+    printf("enter number");
+    scanf("%d", &num);
+    evenOrOdd << <1, 1 >> > (num);
+    cudaDeviceSynchronize();
+    return 0;
+}
+
+
